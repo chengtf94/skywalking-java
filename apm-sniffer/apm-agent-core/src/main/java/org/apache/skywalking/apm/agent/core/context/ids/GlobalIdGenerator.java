b9880b09d15a5eae639071712cd1d42ce2b2b66f
chengtf94 <chengtf94@qq.com>
2024-05-10 01:04:40 +0800

    read code

--- apm-sniffer/apm-agent-core/src/main/java/org/apache/skywalking/apm/agent/core/context/ids/GlobalIdGenerator.java
+++ apm-sniffer/apm-agent-core/src/main/java/org/apache/skywalking/apm/agent/core/context/ids/GlobalIdGenerator.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
 
 package org.apache.skywalking.apm.agent.core.context.ids;
 
@@ -22,7 +5,11 @@ import java.util.UUID;
 
 import org.apache.skywalking.apm.util.StringUtil;
 
+/**
+ * traceId生成器
+ */
 public final class GlobalIdGenerator {
+
     private static final String PROCESS_ID = UUID.randomUUID().toString().replaceAll("-", "");
     private static final ThreadLocal<IDContext> THREAD_ID_SEQUENCE = ThreadLocal.withInitial(
         () -> new IDContext(System.currentTimeMillis(), (short) 0));
@@ -32,15 +19,6 @@ public final class GlobalIdGenerator {
 
     /**
      * Generate a new id, combined by three parts.
-     * <p>
-     * The first one represents application instance id.
-     * <p>
-     * The second one represents thread id.
-     * <p>
-     * The third one also has two parts, 1) a timestamp, measured in milliseconds 2) a seq, in current thread, between
-     * 0(included) and 9999(included)
-     *
-     * @return unique id to represent a trace or segment
      */
     public static String generate() {
         return StringUtil.join(
@@ -91,4 +69,5 @@ public final class GlobalIdGenerator {
             return threadSeq++;
         }
     }
+
 }
